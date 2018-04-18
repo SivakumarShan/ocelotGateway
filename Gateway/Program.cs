995f4c95fa52a3412e626231f3df407ec5be7978
Sivakumar Shanmugavelu <sivakumar.shanmugavelu@boeing.com>
2018-04-18 23:54:51 +0530

    Initial commit

---
+++ Gateway/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .AddCloudFoundry()
+                .UseCloudFoundryHosting()
+                .ConfigureAppConfiguration((builderContext, configBuilder) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+                    configBuilder.SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile("configuration.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                        .AddJsonFile($"configuration.{env.EnvironmentName}.json", optional: true)
+                        .AddEnvironmentVariables()
+                        //.AddConfigServer(env)
+                        .AddCloudFoundry();
+                })
+                .Build();
+    }
+}
---
+++ Gateway/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+using CacheManager.Core;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var audienceConfig = Configuration.GetSection("Audience");
+
+            services.AddOcelot(Configuration)
+                .AddCacheManager(x =>
+                {
+                    x.WithMicrosoftLogging(log =>
+                    {
+                        //log.AddConsole(LogLevel.Debug);
+                    })
+                    .WithDictionaryHandle();
+                });
+            services.AddMvc();
+        }
+
+        public async void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.UseAuthentication();
+            //console logging
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "api/{controller=Home}/{action=Index}/{id?}");
+            });
+
+            await app.UseOcelot();
+        }
+    }
+}
